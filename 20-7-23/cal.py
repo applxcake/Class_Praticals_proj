11e216d3c4b372a4b957dbc99e3725adcfd72733
BlackHawk616 <140146639+BlackHawk616@users.noreply.github.com>
2023-07-21 13:15:47 +0530

    first commit

---
+++ 20-7-23/cal.py
@@ -0,0 +1,51 @@
+from time import sleep # No Need To Use This If Your Using Python IDLE For Scripting
+
+# Program For Addition
+
+print("Welcome To Additon Program\n")
+
+n1 = int(input("Enter Your First Number Here :- "))
+
+n2 = int(input("Enter Your Second Number Here :- "))
+
+add = n1 + n2
+
+print("Your Answer Is :-",add)
+
+# Program For Subtractuion
+
+print("Welcome To Subtraction Program\n")
+
+n3 = int(input("Enter Your First Number Here :- "))
+
+n4 = int(input("Enter Your Second Number Here :- "))
+
+sub = n3 - n4
+
+print("Your Answer Is :-",sub)
+
+# Program FOr Multiplication
+
+print("Welcome To Multiplication Program\n")
+
+n5 = int(input("Enter Your First Number Here :- "))
+
+n6 = int(input("Enter Your Second Number Here :- "))
+
+mul = n5 * n6
+
+print("Your Answer Is :-",mul)
+
+# Programm For Division
+
+print("Welcome To Additon Program\n")
+
+n7 = int(input("Enter Your First Number Here :- "))
+
+n8 = int(input("Enter Your Second Number Here :- "))
+
+div = n7 + n8
+
+print("Your Answer Is :-",div)
+
+sleep(5) # No Need To Use This Unless Ur Running In Terminal 
