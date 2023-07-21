1b5f20b5dd5edfd727d7e91293f5dbdc1ba07cb0
BlackHawk616 <140146639+BlackHawk616@users.noreply.github.com>
2023-07-21 13:47:06 +0530

    first commit

--- 20-7-23/area.py
+++ 20-7-23/area.py
@@ -1,1 +1,37 @@
-#hello 
+from time import sleep # You Guys Dont Need To Use This Feature
+
+#Area Calculator OF Square
+
+print("Welcome To Area Calculator OF Square")
+
+s1 = int(input("Enter The length Of Square :- ")) # Taking input Of Side
+
+areas = s1 * s1
+
+print("Area Of Square is",areas)
+
+# Area Calculator OF Rectangel
+
+print("Welcome To Area Calculator OF rectangle")
+
+l1 = int(input("Enter The Length Of The Rectangle")) # taking input Of Length
+
+b = int(input("Enter The Breatht of The Rectangle")) # Taking Input OF Breath
+
+area = l1 * b
+
+print("Area Of Rectangle Is", area)
+
+# Area OF Circle 
+
+print("Welcome To Area Calculator Of Circle")
+
+r = int(input("Enter The Radius Of The Circle")) # Input Radius OF Circle
+
+pie = 3.14
+
+areac = pie * r * r
+
+print("Area Of Circle is", areas)
+
+sleep(5) # Again You Guys Dont Need To Use This
